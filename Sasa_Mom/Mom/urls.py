f80a688c1debc0a63294333995e1468238f95099
Mangeni-03 <mangeniallan03@gmail.com>
2025-12-04 13:48:05 +0300

    urls and view and adding template to settings.py

--- Sasa_Mom/Mom/urls.py
+++ Sasa_Mom/Mom/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('register/', views.register_mother, name='register'),
+    path('register/success/', views.register_success, name='register_success'),
+    path('dashboard/<int:mother_id>/', views.mother_dashboard, name='dashboard'),
+]
